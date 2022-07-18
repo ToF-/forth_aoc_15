5376ad2ff6e980115c2366d747e69f3436035eb4
Christophe Thibaut <cthibaut@octo.com>
2022-07-18 20:47:40 +0200

    day 3

--- day03/day03.fs
+++ day03/day03.fs
@@ -37,17 +37,38 @@ R constant south
     then
   loop drop ;
 
+variable santa-pos
+variable robot-pos
+variable course-count
+: track-robot-santa-course ( addr,l -- )
+  0 maxpos ! 0 store-pos
+  0 santa-pos ! 0 robot-pos !
+  0 course-count !
+  over + swap do
+    1 course-count +!
+    course-count @ 1 and if santa-pos else robot-pos then
+    dup @ i c@ track-move
+    dup lookup-pos -1 = if
+      dup store-pos
+    then
+    swap !
+  loop ;
+  
 s" >"    track-course maxpos ? cr
 s" ^>v<" track-course maxpos ? cr
 s" ^v^v^v^v^v" track-course maxpos ? cr
 
+s" ^v"    track-robot-santa-course maxpos ? cr
+s" ^>v<" track-robot-santa-course maxpos ? cr
+s" ^v^v^v^v^v" track-robot-santa-course maxpos ? cr
 10000 constant maxline
 create puzzle-line maxline allot
 
 next-arg r/o open-file throw Value fd-in
 puzzle-line maxline fd-in read-line throw drop
 fd-in close-file throw
-puzzle-line swap track-course maxpos ? cr
+puzzle-line over track-course maxpos ? cr
+puzzle-line swap track-robot-santa-course maxpos ? cr
 bye
 
 
