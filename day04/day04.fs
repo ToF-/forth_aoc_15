072454a339c1827b1db93929e6cc6119e8ce4f2e
Christophe Thibaut <cthibaut@octo.com>
2022-07-18 22:00:51 +0200

    day04

--- day04/day04.fs
+++ day04/day04.fs
@@ -3,22 +3,24 @@ require ffl/md5.fs
 md5-create md5hash
 
 
-: has-5z-prefix? ( addr,l -- f )
-  drop 5 over + swap 
+: has-prefix? ( addr,n -- f )
+  dup >r
+  over + swap 
   0 -rot
   do 
     i c@ [char] 0 = if
       1+
     then
-  loop 5 = ;
+  loop r> = ;
 
 : to-string ( n -- addr c )  s>d <# #s #> ;
 
 
-: valid-answer? ( addr,l,n,md5 -- f )
-  .s key drop
-  >r r> drop
-;
+: md5hashstring ( addr,l,n -- f )
+  -rot md5hash md5-reset 
+  md5hash md5-update
+  to-string md5hash md5-update
+  md5hash md5-finish md5+to-string ;
 \    >r
 \    r@ md5-reset
 \    -rot r@ md5-update
@@ -27,7 +29,21 @@ md5-create md5hash
 \    r> drop
 \    has-5z-prefix? ;
 
-s" pqrstuv" 1058970 md5hash dbg valid-answer? .
-s" abcdef"  609043  valid-answer? .
-s" abcdef"  4807    valid-answer? .
+
+variable prefix-size
+
+: solve-it
+  10000000 0 do
+    i 10000 mod 0 = if i . then
+    2dup i md5hashstring drop prefix-size @ has-prefix? if i leave then
+  loop -rot 2drop ;
+
+5 prefix-size !
+s" abcdef" solve-it cr .
+cr
+s" pqrstuv" solve-it cr .
+cr
+s" bgvyzdsv" solve-it cr .
+6 prefix-size !
+s" bgvyzdsv" solve-it cr .
 bye
