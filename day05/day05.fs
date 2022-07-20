e2f4eba91b26ac227bf53f8ecd2d78449274a3b4
Christophe Thibaut <cthibaut@octo.com>
2022-07-20 07:39:05 +0200

    day05 first half with tests

--- day05/day05.fs
+++ day05/day05.fs
@@ -1,39 +1,30 @@
 
+require ffl/tst.fs
+
 : include? ( c,addr,l -- f )
   rot 0 swap 2swap
   over + swap do
-    i c@ over = if
-      nip i swap leave
-    then
+    dup i c@ = if nip i swap leave then
   loop drop ;
 
 : include-3-vowels? ( addr,l -- f)
   0 -rot
-  s" aeiou" over + swap do
-    i c@ >r 2dup r> -rot include? if
-      rot 1+ -rot
-    then
-  loop 2drop 3 >= ;
+  over + swap do
+    i c@ s" aeiou" include? if 1+ then
+  loop 3 >= ;
 
-: include-pair? ( a,b,addr,l -- f)
-  2>r 0 2r>
-  over + swap do           \ a,b,n
-    dup 0= if              \ a,b,n
-      rot dup i c@         \ b,0,a,a,c
-      = if                 \ b,0,a
-        nip 1              \ b,a,1
-      else 
-        -rot               \ a,b,0
-      then
-    else                   \ b,a,1
-      rot dup i c@         \ a,1,b,b,c
-      = if                 \ a,1,b
-        nip 2 leave        \ a,b,2
-      else                 \ a,1,b
-        nip 0              \ a,b,0
-      then
-    then
-  loop -rot 2drop 2 = ;
+: cmp-first ( c1,c2,c1,c -- c2,c1,1 | c1,c2,0 )
+  = if swap 1 else 0 then ;
+
+: include-pair? ( c1,c2,addr,l -- f )
+  0 -rot
+  over + swap do
+    0= if over i c@ cmp-first
+    else over i c@ = if 2 leave
+    else swap over i c@ cmp-first
+    then then
+  loop
+  -rot 2drop 2 = ;
 
 : include-doubled? ( addr,l -- f)
   0 -rot 2dup
@@ -51,7 +42,7 @@
   [char] x [char] y 2swap include-pair? 
   r> or r> or r> or ;
       
-: nice1? ( addr,l -- f )
+: nice-1? ( addr,l -- f )
   2dup include-3-vowels? -rot
   2dup include-doubled? -rot
   include-forbidden? 0= and and ;
@@ -67,52 +58,63 @@ next-arg file-name-size ! file-name !
 
 0 value fd-in
 
-: solve-it1
+: solve-it-1
   0 file-name @ file-name-size @ r/o open-file throw to fd-in
   begin
     puzzle-line maxline fd-in read-line throw while
-    puzzle-line swap 2dup type nice1? if 1+ ."  Y" else ."  N" then cr 
+    puzzle-line swap nice-1? if 1+ then
   repeat drop
   fd-in close-file throw ;
   
 page
-solve-it1 .
-cr
-s" kpvwblrizaabmnhz" include-3-vowels? . cr
-s" pygmy" include-3-vowels? . cr
-s" dog" include-3-vowels? . cr
-s" pygmalion" include-3-vowels? . cr
-cr
-bye
-s" pygmalion" char y -rot include? . cr
-s" pygmalion" char z -rot include? . cr
-s" pygmalion" char n -rot include? . cr
-cr
-s" pygmalion" char g char u 2swap include-pair? . cr
-s" pygmalion" char g char a 2swap include-pair? . cr
-s" pygmalion" char l char a 2swap include-pair? . cr
-s" domino"    char o char o 2swap include-pair? . cr
-s" pygmalion" char g char m 2swap include-pair? . cr
-s" pygmalion" char o char n 2swap include-pair? . cr
-s" pygmalion" char p char y 2swap include-pair? . cr
 
-cr
-s" domino" include-doubled? . cr
-s" ardvaark" include-doubled? . cr
-s" waterloo" include-doubled? . cr
-cr
-s" domino" include-forbidden? . cr
-s" abcde" include-forbidden? . cr
-s" macdonald" include-forbidden? . cr
-s" cupqake" include-forbidden? . cr
-s" maxymum" include-forbidden? . cr
-
-cr
-s" ugknbfddgicrmopn" nice1? . cr
-s" aaa" nice1? . cr
-s" jchzalrnumimnmhp" nice1? . cr
-s" haegwjzuvuyypxyu" nice1? . cr
-s" dvszwmarrgswjxmb" nice1? . cr
+t{ ." include?" cr
+  s" pygmalion" char y -rot include? ?true
+  s" pygmalion" char z -rot include? ?false
+  s" pygmalion" char n -rot include? ?true
+}t
+t{ ." include-pair?" cr
+  s" pygmalion" char g char u 2swap include-pair? ?false
+  s" pygmalion" char g char a 2swap include-pair? ?false
+  s" pygmalion" char l char a 2swap include-pair? ?false
+  s" domino"    char o char o 2swap include-pair? ?false
+  s" pygmalion" char p char y 2swap include-pair? ?true
+  s" pygmalion" char g char m 2swap include-pair? ?true
+  s" pygmalion" char o char n 2swap include-pair? ?true
+  s" aabmnh" char a char b 2swap include-pair? ?true
+}t
+t{ ." include-3-vowels?" cr
+  s" kpvwblrizaabmnhz" include-3-vowels? ?true
+  s" pygmy" include-3-vowels? ?false
+  s" dog" include-3-vowels? ?false
+  s" pygmalion" include-3-vowels? ?true
+}t
+t{ ." include-doubled?" cr
+  s" domino" include-doubled? ?false
+  s" ardvaark" include-doubled? ?true
+  s" waterloo" include-doubled? ?true
+}t
+t{ ." include-forbidden?" cr
+  s" domino" include-forbidden? ?false
+  s" abhorrent" include-forbidden? ?true
+  s" cabdriver" include-forbidden? ?true
+  s" abcde" include-forbidden? ?true
+  s" macdonald" include-forbidden? ?true
+  s" cupqake" include-forbidden? ?true
+  s" maxymum" include-forbidden? ?true
+  s" aabmnhz" include-forbidden? ?true
+  s" kpvwblrizaabmnhz" include-forbidden? ?true
+}t
+t{ ." nice-1?" cr
+  s" ugknbfddgicrmopn" nice-1? ?true
+  s" aaa" nice-1? ?true
+  s" jchzalrnumimnmhp" nice-1? ?false
+  s" haegwjzuvuyypxyu" nice-1? ?false
+  s" dvszwmarrgswjxmb" nice-1? ?false
+}t
+t{ ." solve-it-1" cr
+  solve-it-1 255 ?s
+}t
 
 bye
 
