9e2e037bba24db64c60f5f9073ece12e36cb3fef
Christophe Thibaut <cthibaut@octo.com>
2022-07-24 09:29:26 +0200

    day 07 wip

--- day07/day07.fs
+++ day07/day07.fs
@@ -1,27 +1,41 @@
 
-0 constant NULL-TOKEN
-1 constant LITT-TOKEN
-2 constant VAR-TOKEN
-3 constant NOT-TOKEN
-4 constant AND-TOKEN
-5 constant OR-TOKEN
-6 constant LS-TOKEN
-7 constant RS-TOKEN
+0 constant NULL-OP
+1 constant LITT-OP
+2 constant VAR-OP
+3 constant NOT-OP
+4 constant AND-OP
+5 constant OR-OP
+6 constant LS-OP
+7 constant RS-OP
+8 constant ARROW-OP
 
 65535 constant 16bits-mask
 
 1000 constant max-tokens
-variable last-token
+variable #tokens
 
 create tokens max-tokens cells allot
-0 last-token !
+0 #tokens !
 
-create terms max-tokens 10 cells * allot
+create terms max-tokens cells allot
+variable #terms
+0 #terms !
 
+create term-strings max-tokens cells 10 * allot
+variable current-string
+term-strings current-string !
+
+: init
+  #tokens off
+  #terms off
+  term-strings current-string ! ;
+
+: last-token# ( -- n )
+  #tokens @ 1- ;
 
 : add-token ( t -- )
-  tokens last-token @ cells + !
-  1 last-token +! ;
+  tokens #tokens @ cells + !
+  1 #tokens +! ;
 
 : >>op-kind ( v -- op )
   32 rshift ;
@@ -47,10 +61,10 @@ create terms max-tokens 10 cells * allot
   <<op-kind or add-token ; 
 
 : make-variable ( t -- )
-  VAR-TOKEN <<op-kind or add-token ;
+  VAR-OP <<op-kind or add-token ;
 
 : make-constant ( t -- )
-  LITT-TOKEN <<op-kind or add-token ;
+  LITT-OP <<op-kind or add-token ;
 
 : eval-litt ( v -- n )
   16bits-mask and ;
@@ -63,28 +77,28 @@ create terms max-tokens 10 cells * allot
 
 : eval-token ( v -- )
   dup >>op-kind
-  dup LITT-TOKEN = if
+  dup LITT-OP = if
     drop 16bits-mask and 
   else 
-    dup VAR-TOKEN = if
+    dup VAR-OP = if
       drop operand1 token recurse
     else
-      dup NOT-TOKEN = if
+      dup NOT-OP = if
         drop operand1 token recurse not
       else 
         swap
         dup operand1 token recurse
         swap operand2 token recurse
-        rot dup AND-TOKEN = if
+        rot dup AND-OP = if
           drop and
         else 
-          dup OR-TOKEN = if
+          dup OR-OP = if
             drop or
           else
-            dup LS-TOKEN = if
+            dup LS-OP = if
               drop lshift
             else
-              dup RS-TOKEN = if
+              dup RS-OP = if
                 drop rshift
               else 
                 s" unknown operator" exception throw
@@ -95,12 +109,79 @@ create terms max-tokens 10 cells * allot
       then
     then
   then ;
-  
+
+: advance-current-string
+  current-string @ count +
+  current-string ! ;
+
+: new-term-string
+  current-string @ 
+  terms #terms @ cells + !
+  1 #terms +! ;
+
+: add-term-string ( addr,l -- )
+  dup -rot 
+  current-string @ 1+ swap
+  cmove 
+  current-string @ c!
+  new-term-string
+  advance-current-string ;
+
 : find-term ( addr,l -- n,T|F )
-  false -rot
-  last-token @ 0 ?do
-    2dup i 10 cells * terms + equals? if
-      rot drop i true 2swap leave
-    then
-  loop 2drop ;
+  2>r
+  0 term-strings
+  begin
+    dup current-string @ < 
+    over count 2r@ compare and while
+      count +
+      swap 1+ swap
+  repeat
+  2r> 2drop
+  current-string @ < if
+    true
+  else
+    drop false
+  then ;
   
+: find-operator ( addr,l -- op )
+  2dup s" NOT" compare 0= if
+    NOT-OP
+  else
+    2dup s" AND" compare 0= if
+      AND-OP
+    else
+      2dup s" OR" compare 0= if
+        OR-OP
+      else
+        2dup s" LSHIFT" compare 0= if
+          LS-OP
+        else
+          2dup s" RSHIFT" compare 0= if
+            RS-OP
+          else
+            2dup s" ->" compare 0= if
+              ARROW-OP
+            else
+              NULL-OP
+            then
+          then
+        then
+      then
+    then
+  then -rot 2drop ;
+
+: assign-variable ( t,addr,l -- )
+  2dup find-term if s" already assigned" exception throw then
+  add-term-string
+  make-variable ;
+
+: find-value ( addr,l -- t )
+  2dup find-term 0= if
+    2dup s>number? if
+      d>s make-constant
+      add-term-string
+      last-token#
+    then
+  else
+    -rot 2drop
+  then ;
--- day07/day07test.fs
+++ day07/day07test.fs
@@ -3,18 +3,21 @@ require day07.fs
 
 page
 t{ ." an expression" cr
+  init
+  last-token# -1 ?s
   123 make-constant           \ 0: 123
   0 make-variable             \ 123 -> 1: x
   456 make-constant           \ 2: 456
   2 make-variable             \ 456 -> 3: y
-  1 3 AND-TOKEN make-binany-op  \ x AND y -> 4: d
-  1 3 OR-TOKEN make-binany-op   \ x OR y -> 5: e
+  1 3 AND-OP make-binany-op  \ x AND y -> 4: d
+  1 3 OR-OP make-binany-op   \ x OR y -> 5: e
   2 make-constant             \ 6: 2
-  1 6 LS-TOKEN make-binany-op  \ x LSHIFT 2 -> 7: f
-  3 6 RS-TOKEN make-binany-op  \ y RSHIFT 2 -> 8: g
-  1 NOT-TOKEN make-unary-op    \ NOT x -> 9: h
-  3 NOT-TOKEN make-unary-op    \ NOT y -> 10: i
-  last-token @ 11 ?s
+  1 6 LS-OP make-binany-op  \ x LSHIFT 2 -> 7: f
+  3 6 RS-OP make-binany-op  \ y RSHIFT 2 -> 8: g
+  1 NOT-OP make-unary-op    \ NOT x -> 9: h
+  3 NOT-OP make-unary-op    \ NOT y -> 10: i
+  #tokens @ 11 ?s
+  last-token# 10 ?s
   0 token eval-token 123 ?s
   1 token eval-token 123 ?s
   2 token eval-token 456 ?s
@@ -29,7 +32,41 @@ t{ ." an expression" cr
 }t
 
 t{ ." find-term" cr
-  0 last-token !
-  s" 123" find-term 0 ?s
+  init
+  s" foo" find-term ?false
+  s" foo" add-term-string
+  s" foo" find-term ?true 0 ?s
+  s" bar" add-term-string
+  s" qux" add-term-string
+  s" bar" find-term ?true 1 ?s
+  s" qux" find-term ?true 2 ?s
+  s" baz" find-term ?false
+}t
+
+t{ ." find-operator" cr
+  s" FOO" find-operator NULL-OP ?s
+  s" AND" find-operator AND-OP ?s
+  s" ->"  find-operator ARROW-OP ?s
+}t
+
+t{ ." assign-variable" cr
+  init
+  123 make-constant 
+  #tokens @ 1 ?s
+  last-token# s" x" assign-variable
+  #tokens @ 2 ?s
+  1 token eval-token 123 ?s
+  last-token# s" y" assign-variable
+  #tokens @ 3 ?s
+  last-token# token eval-token 123 ?s
+}t
+
+t{ ." find-value" cr
+  init
+  s" 123" find-value token eval-token 123 ?s
+  s" 456" find-value token eval-token 456 ?s
+  s" 123" find-value token eval-token 123 ?s
+  s" 456" find-value token eval-token 456 ?s
+  #tokens @ 2 ?s
 }t
 bye
