b41a8fa5a638c35f987a4dc04be4073c7cbcaddd
Christophe Thibaut <cthibaut@octo.com>
2022-07-28 00:11:11 +0200

    first stab at instruction > forth

--- day07/sar.fs
+++ day07/sar.fs
@@ -11,11 +11,19 @@ create output-ref 10 allot
 create signals 30 allot
 variable steps#
 
+create forth-instruction-ref 80 allot
+
+: init-forth-instruction
+  forth-instruction-ref 80 erase ;
+
+: forth-instruction ( -- addr,l )
+  forth-instruction-ref count ;
+
 create operators 80 allot
 operators 80 erase
 
 : init-operators
-  s\" \3NOT\3AND\2OR\6LSHIFT\6RSHIFT\2->"
+  s\" \3NOT\3AND\2OR\6LSHIFT\6RSHIFT\2->\5 NOT \5 AND \4 OR \8 LSHIFT \8 RSHIFT \4 -> "
   0 do
     dup i + c@
     operators i + c!
@@ -29,16 +37,29 @@ init-operators
 : operator ( n -- )
   operators nth-string ;
 
-: is-operator? ( addr,l -- n,T|F )
-  false -rot 6 0 do 
+: is-operator? ( addr,l -- f )
+  false -rot 12 0 do 
     i operator count 2over compare 0= if
-      rot drop i true 2swap leave
+      rot drop true -rot leave
     then
   loop 
   2drop ;
 
-: is-number? ( addr,l -- )
-  s>unumber?  if d>s true else 2drop false then ;
+: is-digit? ( c -- f )
+  dup [char] 0 >= swap [char] 9 <= and ;
+
+: is-number? ( addr,l -- f )
+  true -rot
+  over + swap do
+    i c@ dup is-digit? 0= if
+      bl <> if drop false then
+    else 
+      drop
+    then
+  loop ;
+
+: is-symbol? ( addr,l -- f )
+  2dup is-number? -rot is-operator? or 0= ;
 
 : s>copy ( addr,l,dest -- )
   over over c!
@@ -53,31 +74,36 @@ init-operators
 : nth-step ( n -- addr,l )
     nth-step-ref count ;
 
-: ?nip ( n,T|F -- f )
-  dup if nip then ;
-
 : nth-step-cmove ( addr,l,n -- )
   >r 
   2dup 
-  2dup is-number?   ?nip
-  -rot is-operator? ?nip or 
+  2dup is-number?   
+  -rot is-operator? or 
   r> swap if
-    nth-step-ref s>copy
+    -rot
+    s"  " pad place
+    pad +place
+    s"  " pad +place
+    pad count
+    rot nth-step-ref s>copy
   else
     -rot
     s" _" pad place
     pad +place
-    s" _" pad +place
+    s" _ " pad +place
     pad count
     rot nth-step-ref s>copy
   then ;
+
+: nth-step>copy ( addr,l,n -- )
+  over if nth-step-cmove else drop 2drop then ;
 
 : parse-names ( n -- n )
   0 swap
   0 do
     parse-name 
     dup if rot 1+ -rot then
-    i nth-step-cmove
+    i nth-step>copy
   loop 
   dup steps# ! ;
 
@@ -86,7 +112,8 @@ init-operators
   s" 5 parse-names " pad place
   pad +place
   s\" \n" pad +place
-  pad count evaluate ;
+  pad count 
+  evaluate ;
 
 : step>output ( n -- )
   output-ref s>copy ;
@@ -101,7 +128,7 @@ init-operators
 : output ( -- addrl,l )
   output-ref count ;
 
-: step>signals ( n -- )
+: arrange-steps ( n -- )
   dup 3 = if drop
     0 nth-step 0 signal-ref s>copy
     2 nth-step output-ref s>copy
@@ -115,6 +142,17 @@ init-operators
     1 nth-step 2 signal-ref s>copy
     4 nth-step output-ref s>copy
   then then ;
+
+: instruction>forth ( addr,l -- )
+  instruction>steps
+  arrange-steps
+  init-forth-instruction
+  s" : " pad place
+  output pad +place
+  0 signal pad +place
+  s" ;" pad +place
+  pad count forth-instruction-ref s>copy
+  ;
 
 : symbol ( addr,l -- addr,l )
   s" _" pad place
--- day07/sartest.fs
+++ day07/sartest.fs
@@ -1,58 +1,74 @@
 require ffl/tst.fs
 require sar.fs
 page
-t{ ." nth-step-cmove" cr
+t{ ." is-operator?" cr
+  s" AND" is-operator? ?true
+  s" ->" is-operator? ?true
+  s" 123" is-operator? ?false
+}t
 
-  s" 123" 0 nth-step-cmove 
-  0 nth-step s" 123" ?str
+t{ ." is-number?" cr
+  s" 123" is-number? ?true 
+  s" foo" is-number? ?false 
+}t
 
-  s" AND" 1 nth-step-cmove  
-  1 nth-step s" AND" ?str 
+t{ ." is-symbol?" cr
+  s"  123 " is-symbol? ?false
+  s"  AND " is-symbol? ?false
+  s" _foo_ " is-symbol? ?true
+}t
 
-  s" foo" 2 nth-step-cmove
-  2 nth-step s" _foo_" ?str
+t{ ." nth-step>copy" cr
+
+  s" 123" 0 nth-step>copy 
+  0 nth-step s"  123 " ?str
+
+  s" AND" 1 nth-step>copy  
+  1 nth-step s"  AND " ?str 
+
+  s" foo" 2 nth-step>copy
+  2 nth-step s" _foo_ " ?str
+
+  s" " 3 nth-step>copy
+  3 nth-step s" " ?str
+
 }t
 
 t{ ." instruction>steps" cr
   s" foo AND 4807 -> baz" instruction>steps
   5 ?s
 
-  0 nth-step s" _foo_" ?str
-  1 nth-step s" AND" ?str
-  2 nth-step s" 4807" ?str
-  3 nth-step s" ->" ?str
-  4 nth-step s" _baz_" ?str
+  0 nth-step s" _foo_ " ?str
+  1 nth-step s"  AND " ?str
+  2 nth-step s"  4807 " ?str
+  3 nth-step s"  -> " ?str
+  4 nth-step s" _baz_ " ?str
 
   s" foo bar quux" instruction>steps 3 ?s
 }t
 
-t{ ." is-operator?" cr
-  s" AND" is-operator? ?true 1 ?s
-  s" ->" is-operator? ?true 5 ?s
-  s" 123" is-operator? ?false
-}t
-
-t{ ." is-number" cr
-  s" 123" is-number? ?true 123 ?s
-  s" foo" is-number? ?false 
-}t
-
 t{ ." arrange-steps" cr
 
-  s" 123 -> x" instruction>steps step>signals
-  output s" _x_" ?str
-  0 signal s" 123" ?str
+  s" 123 -> x" instruction>steps arrange-steps
+  output s" _x_ " ?str
+  0 signal s"  123 " ?str
 
-  s" 123 AND 456 -> foo" instruction>steps step>signals
-  output s" _foo_" ?str
-  0 signal s" 123" ?str
-  1 signal s" 456" ?str
-  2 signal s" AND" ?str
+  s" 123 AND 456 -> foo" instruction>steps arrange-steps
+  output s" _foo_ " ?str
+  0 signal s"  123 " ?str
+  1 signal s"  456 " ?str
+  2 signal s"  AND " ?str
 
-  s" NOT 123 -> foo" instruction>steps step>signals
-  output s" _foo_" ?str
-  0 signal s" 123" ?str
-  1 signal s" NOT" ?str
+  s" NOT 123 -> foo" instruction>steps arrange-steps
+  output s" _foo_ " ?str
+  0 signal s"  123 " ?str
+  1 signal s"  NOT " ?str
 }t
 
+t{ ." instruction>forth" 
+  s" 123 -> x" instruction>forth 
+  forth-instruction s" : _x_  123 ;" ?str \ because _x_ can't be found
+  \ if _x_ can be found it has been deferred
+  \ so we should have s" : >_x_ 123 ; ' >_x_ is _x_ "
+}t
 bye
