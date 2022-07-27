e8dc929e37299b1bae20a1f38c0efdeb160aef73
Christophe Thibaut <cthibaut@octo.com>
2022-07-27 23:11:22 +0200

    arrange steps

--- day07/sar.fs
+++ day07/sar.fs
@@ -7,6 +7,42 @@
 10 constant step-size
 step-size max-steps * constant steps-size
 create steps steps-size allot
+create output-ref 10 allot
+create signals 30 allot
+variable steps#
+
+create operators 80 allot
+operators 80 erase
+
+: init-operators
+  s\" \3NOT\3AND\2OR\6LSHIFT\6RSHIFT\2->"
+  0 do
+    dup i + c@
+    operators i + c!
+  loop drop ;
+
+init-operators
+
+: nth-string ( n,addr -- addr )
+  swap 0 ?do count + loop ;
+
+: operator ( n -- )
+  operators nth-string ;
+
+: is-operator? ( addr,l -- n,T|F )
+  false -rot 6 0 do 
+    i operator count 2over compare 0= if
+      rot drop i true 2swap leave
+    then
+  loop 
+  2drop ;
+
+: is-number? ( addr,l -- )
+  s>unumber?  if d>s true else 2drop false then ;
+
+: s>copy ( addr,l,dest -- )
+  over over c!
+  1+ swap cmove ;
 
 : init-steps
     steps step-size erase ;
@@ -17,10 +53,24 @@ create steps steps-size allot
 : nth-step ( n -- addr,l )
     nth-step-ref count ;
 
+: ?nip ( n,T|F -- f )
+  dup if nip then ;
+
 : nth-step-cmove ( addr,l,n -- )
-    nth-step-ref
-    over over c!
-    1+ swap cmove ;
+  >r 
+  2dup 
+  2dup is-number?   ?nip
+  -rot is-operator? ?nip or 
+  r> swap if
+    nth-step-ref s>copy
+  else
+    -rot
+    s" _" pad place
+    pad +place
+    s" _" pad +place
+    pad count
+    rot nth-step-ref s>copy
+  then ;
 
 : parse-names ( n -- n )
   0 swap
@@ -28,7 +78,8 @@ create steps steps-size allot
     parse-name 
     dup if rot 1+ -rot then
     i nth-step-cmove
-  loop ;
+  loop 
+  dup steps# ! ;
 
 : instruction>steps ( addr,l -- )
   init-steps
@@ -37,6 +88,33 @@ create steps steps-size allot
   s\" \n" pad +place
   pad count evaluate ;
 
+: step>output ( n -- )
+  output-ref s>copy ;
+
+
+: signal-ref ( n -- addr )
+  signals nth-string ;
+
+: signal ( n -- addr,l )
+  signal-ref count ;
+
+: output ( -- addrl,l )
+  output-ref count ;
+
+: step>signals ( n -- )
+  dup 3 = if drop
+    0 nth-step 0 signal-ref s>copy
+    2 nth-step output-ref s>copy
+  else 4 = if
+    1 nth-step 0 signal-ref s>copy
+    0 nth-step 1 signal-ref s>copy
+    3 nth-step output-ref s>copy
+  else 
+    0 nth-step 0 signal-ref s>copy
+    2 nth-step 1 signal-ref s>copy
+    1 nth-step 2 signal-ref s>copy
+    4 nth-step output-ref s>copy
+  then then ;
 
 : symbol ( addr,l -- addr,l )
   s" _" pad place
--- day07/sartest.fs
+++ day07/sartest.fs
@@ -1,15 +1,58 @@
 require ffl/tst.fs
 require sar.fs
 page
-t{ ." instruction>steps" cr
+t{ ." nth-step-cmove" cr
 
-  s" foo bar quux bar baz" instruction>steps
+  s" 123" 0 nth-step-cmove 
+  0 nth-step s" 123" ?str
+
+  s" AND" 1 nth-step-cmove  
+  1 nth-step s" AND" ?str 
+
+  s" foo" 2 nth-step-cmove
+  2 nth-step s" _foo_" ?str
+}t
+
+t{ ." instruction>steps" cr
+  s" foo AND 4807 -> baz" instruction>steps
   5 ?s
-  0 nth-step s" foo" ?str
-  2 nth-step s" quux" ?str
-  4 nth-step s" baz" ?str
+
+  0 nth-step s" _foo_" ?str
+  1 nth-step s" AND" ?str
+  2 nth-step s" 4807" ?str
+  3 nth-step s" ->" ?str
+  4 nth-step s" _baz_" ?str
 
   s" foo bar quux" instruction>steps 3 ?s
+}t
+
+t{ ." is-operator?" cr
+  s" AND" is-operator? ?true 1 ?s
+  s" ->" is-operator? ?true 5 ?s
+  s" 123" is-operator? ?false
+}t
+
+t{ ." is-number" cr
+  s" 123" is-number? ?true 123 ?s
+  s" foo" is-number? ?false 
+}t
+
+t{ ." arrange-steps" cr
+
+  s" 123 -> x" instruction>steps step>signals
+  output s" _x_" ?str
+  0 signal s" 123" ?str
+
+  s" 123 AND 456 -> foo" instruction>steps step>signals
+  output s" _foo_" ?str
+  0 signal s" 123" ?str
+  1 signal s" 456" ?str
+  2 signal s" AND" ?str
+
+  s" NOT 123 -> foo" instruction>steps step>signals
+  output s" _foo_" ?str
+  0 signal s" 123" ?str
+  1 signal s" NOT" ?str
 }t
 
 bye
