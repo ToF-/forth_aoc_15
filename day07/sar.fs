c38537fca1c3cffbdbf943101d3ee9076fa2a0a5
Christophe Thibaut <cthibauttof@gmail.com>
2022-07-27 09:26:13 +0200

    parsing instructions

--- day07/sar.fs
+++ day07/sar.fs
@@ -1,3 +1,43 @@
+5 constant max-steps
+10 constant step-size
+step-size max-steps * constant steps-size
+create steps steps-size allot
+
+: init-steps
+    steps step-size erase ;
+
+: nth-step-ref ( n, addr )
+    step-size * steps + ;
+
+: nth-step ( n -- addr,l )
+    nth-step-ref count ;
+
+: nth-step-cmove ( addr,l,n -- )
+    nth-step-ref
+    over over c!
+    1+ swap cmove ;
+
+: parse-names ( n -- addr,l,addr,l.. )
+  0 do
+    parse-name 
+    i nth-step-cmove
+  loop ;
+
+: instruction>steps ( addr,l -- )
+  init-steps
+  s" 5 parse-names " pad place
+  pad +place
+  s\" \n" pad +place
+  pad count evaluate ;
+
+s" foo bar quux" instruction>steps 
+0 nth-step type cr
+1 nth-step type cr
+2 nth-step type cr
+3 nth-step type cr
+4 nth-step type cr
+bye
+
 
 : symbol ( addr,l -- addr,l )
   s" _" pad place
