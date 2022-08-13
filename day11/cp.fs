0bd42d0b3a96b2f5d089ec3c7f8009a4843bbd67
Christophe Thibaut <cthibauttof@gmail.com>
2022-08-13 12:08:03 +0200

    day11

--- day11/cp.fs
+++ day11/cp.fs
@@ -1,13 +1,13 @@
 \ Corporate Policy 
+char z constant z
+char a constant a
 
 : (increment) ( addr,addr' -- )
   2dup <= if
-    dup
-    c@ dup [char] z < if
+    dup c@ dup z < if
       1+ swap c! drop
     else
-      drop [char] a
-      over c!
+      drop a over c!
       1- recurse
     then
   else
@@ -16,3 +16,69 @@
 
 : increment ( addr,l -- )
   1- over + (increment) ;
+
+: straight? ( a,b,c -- f )
+  over - 1 = -rot 
+  swap - 1 = and ;
+
+: (has-straight) ( addr,addr' -- f )
+  2dup swap - 2 >= if
+    dup 
+    dup 1- dup 1-
+    c@ swap c@ rot c@ straight? if
+      2drop true
+    else
+      1- recurse
+    then
+  else
+    2drop false
+  then ;
+
+: has-straight ( addr,l -- f )
+  1- over + (has-straight) ;
+
+: iol? ( c -- f )
+  dup [char] i =
+  swap dup [char] o =
+  swap [char] l = or or ;
+
+
+: has-letters-iol ( addr,l -- f)
+  false -rot
+  bounds do
+    i c@ iol? if drop true leave then
+  loop ;
+
+: has-one-pair ( addr,l -- c,T|F )
+  false -rot
+  1- bounds do
+    i dup 1+ c@ swap c@ = if
+      drop i c@ true leave
+    then
+  loop ;
+
+: has-one-different-pair ( c,addr,l -- f )
+  1- bounds do
+    i dup 1+ c@ swap c@ = 
+    over i c@ <> and if
+      drop true leave
+    then
+  loop true = ;
+
+: has-two-pairs ( addr,l -- f )
+  2dup has-one-pair if
+    -rot has-one-different-pair
+  else
+    2drop false
+  then ;
+
+: meet-requirements ( addr,l -- f )
+  2dup has-straight -rot
+  2dup has-letters-iol 0= -rot
+  has-two-pairs and and ;
+
+: next-password ( addr,l -- )
+  begin
+    2dup increment
+    2dup meet-requirements 0= while
+  repeat 2drop ;
--- day11/cptest.fs
+++ day11/cptest.fs
@@ -14,4 +14,42 @@ t{ ." incrementing string" cr
   s" aaaaaaaa" ?str
 }t
 
+t{ ." has-straight" cr
+  s" aaaaaabc" has-straight ?true
+  s" aaaaabca" has-straight ?true
+  s" abcdazta" has-straight ?true
+  s" xyzdazta" has-straight ?true
+  s" aacaaaaa" has-straight ?false
+}t
+
+t{ ." has-letters-iol" cr
+  s" aaaaaaaa" has-letters-iol ?false
+  s" aaaoaaaa" has-letters-iol ?true
+  s" aaaaaaia" has-letters-iol ?true
+  s" laaaaaaa" has-letters-iol ?true
+}t
+
+t{ ." has-two-pairs" cr
+  s" aaaaaaaa" has-two-pairs ?false
+  s" aabbaaaa" has-two-pairs ?true
+  s" abbdefhh" has-two-pairs ?true
+  s" abbdefbb" has-two-pairs ?false
+  s" abbbefhh" has-two-pairs ?true
+}t
+
+t{ ." meet-requirements" cr
+  s" hijklmmn" meet-requirements ?false
+  s" abbceffg" meet-requirements ?false
+  s" abbcegjk" meet-requirements ?false
+  s" abcdffaa" meet-requirements ?true
+  s" ghjaabcc" meet-requirements ?true
+}t
+
+t{ ." next-password" cr
+  s" abcdefgh" 2dup next-password s" abcdffaa" ?str
+  s" ghijklmn" 2dup next-password s" ghjaabcc" ?str
+  s" hxbxwxba" 2dup next-password s" hxbxxyzz" ?str
+  s" hxbxxyzz" 2dup next-password s" hxcaabcc" ?str
+}t
+
 bye
