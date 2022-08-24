ed6557af2b6bd4f62bfd2abccfbe10459bb6f1ee
Christophe Thibaut <cthibauttof@gmail.com>
2022-08-24 16:00:42 +0200

    day12 wip

---
+++ day12/jasftest.fs
@@ -0,0 +1,47 @@
+require ffl/tst.fs
+require jsaf.fs
+
+page
+
+t{ ." extract-number" cr
+  s" " extract-number 0 ?s
+  s" 42" extract-number 42 ?s
+  s" [[23]]" extract-number 23 ?s
+  s" -49" extract-number -49 ?s
+}t
+
+t{ ." sum sample" cr
+  s" sample.txt" sum 9 ?s
+}t
+
+t{ ." sum puzzle" cr
+  s" puzzle12.txt" sum 156366 ?s
+}t
+t{ ." opening an object" cr
+  s" {" parse-json swap JS-OBJECT ?s 0 ?s
+}t
+t{ ." closing an object" cr
+  s" { }" parse-json 0 ?s
+}t
+t{ ." opening a list" cr
+  s" [" parse-json swap JS-LIST ?s 0 ?s
+}t
+t{ ." closing a list" cr
+  s" [ ]" parse-json 0 ?s
+}t
+t{ ." in-a-list?" cr
+  s" {[ " parse-json in-a-list? ?true
+  s" ]" parse-json in-a-list? ?false
+  clearstack
+}t
+t{ ." numeric value" cr
+  s" [ 4807 " parse-json 4807 ?s swap JS-LIST ?s 0 ?s
+  s" [ -2317 " parse-json -2317 ?s swap JS-LIST ?s 0 ?s
+}t
+t{ ." string" cr
+  s\" [\"foo" parse-json in-a-string? ?true
+  s\" [\"foo\"" parse-json in-a-string? ?false
+  clearstack
+}t
+
+bye
---
+++ day12/jsaf.fs
@@ -0,0 +1,134 @@
+\ JSabacusFramework.io
+
+1   constant JS-LIST
+2   constant JS-OBJECT
+4   constant JS-STRING
+128 constant RED
+3 constant str
+4 constant nbr
+
+80 constant string-max
+create current-string-value string-max
+: openJS-OBJECTect ( -- O,0 )
+  JS-OBJECT 0 ;
+
+: closeJS-OBJECTect ( C,n -- n|0 )
+  swap red = if
+    drop 0
+  then ;
+
+: openList ( -- C,0 )
+  JS-LIST 0 ;
+
+: closeList ( -- n )
+  nip ;
+
+: comma ( C,n -- C,n' )
+  + ;
+
+: quote ( C,n -- C,n )
+;
+
+: in-a-list? ( ... -- f )
+  over JS-LIST = ;
+
+: is-digit? ( c -- f )
+  dup [char] 0 >=
+  swap [char] 9 <= and ;
+
+: is-minus? ( c -- f )
+  [char] - = ;
+
+: (js-number) ( addr -- n,addr' )
+  0 swap
+  begin
+    dup c@ dup is-digit? while
+      [char] 0 - 
+      rot 10 * + swap
+    1+
+  repeat drop ;
+
+: js-number ( addr -- n,l )
+  dup 
+  dup c@ is-minus? dup if swap 1+ swap then >r
+  (js-number)
+  r> if swap negate swap then
+  swap -rot swap - ;
+
+: is-quote? ( c -- f )
+  [char] " = ;
+
+: in-a-string? ( ... -- f )
+  over JS-STRING = ;
+
+variable last-string
+variable last-string-len
+
+: parse-json ( addr,l -- ... )
+  bounds do
+    i c@ dup [char] { = if
+      drop JS-OBJECT 0
+      1
+    else dup [char] } = if
+      drop
+      swap assert( JS-OBJECT or )
+      1
+    else dup [char] [ = if
+      drop JS-LIST 0 
+      1
+    else dup [char] ] = if
+      drop 
+      swap assert( JS-LIST = )
+      1
+    else dup is-digit? over is-minus? or if
+      drop
+      i js-number 
+    else dup [char] " = if
+      in-a-string? 0= if
+        drop JS-STRING 0
+        i last-string !
+        last-string-len off
+        1
+      else
+        drop assert (JS-STRING = )
+        last-string @ last-string-len @ s" red" compare 0= if
+        then
+         
+      then
+    else
+      drop
+      1
+    then then then then then then
+  +loop ;
+
+: extract-number ( addr,l -- n )
+  false 0 2swap
+  ?dup if
+    bounds do
+      i c@ dup is-digit? if
+        [char] 0 - 
+        swap 10 * +
+      else
+        is-minus? if
+          nip true swap
+        then
+      then
+    loop
+  else
+    drop
+  then
+  swap if negate then ;
+
+80 constant line-size
+create line line-size allot
+
+0 value fd-in
+
+: sum ( addr,l -- n )
+  r/o open-file throw to fd-in
+  0
+  begin
+    line line-size fd-in read-line throw while
+    line swap extract-number +
+  repeat
+  fd-in close-file throw drop ;
