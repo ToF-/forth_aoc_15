9d3828bbcfe2b7e5434866e0f2f1fb683b1a900b
Christophe Thibaut <cthibaut@octo.com>
2022-08-27 14:07:12 +0200

    day13 wip

--- day13/kdt.fs
+++ day13/kdt.fs
@@ -4,9 +4,23 @@
 variable #guests
 
 create guests max-guests dup * cells allot
+create arrangement max-guests cells allot
 
 : happiness! ( i,j,n -- )
   rot max-guests * rot + cells guests + ! ;
 
 : happiness ( i,j -- n )
   swap max-guests * + cells guests + @ ;
+
+: global-happiness ( -- n )
+  0
+  #guests @ 0 do
+    i cells arrangement + @ 
+    dup 
+    dup  1- #guests @ mod cells arrangement + @ happiness
+    -rot
+    dup 1+ #guests @ mod cells arrangement + @ happiness
+    + +
+  loop ;
+  
+
--- day13/test.fs
+++ day13/test.fs
@@ -3,6 +3,7 @@ require kdt.fs
 
 page
 t{ ." sample happiness relation" cr
+  4 #guests !
   0   1  54 happiness!
   0   2 -59 happiness!
   0   3  -2 happiness!
@@ -19,6 +20,18 @@ t{ ." sample happiness relation" cr
   0 0 happiness 0 ?s
   0 1 happiness 54 ?s
   2 3 happiness 55 ?s
+}t
+t{ ." arrangement global happiness" cr
+  2  arrangement 0 cells + !  \ 60+55
+  3  arrangement 1 cells + !  \ 41+46
+  0  arrangement 2 cells + !  \ -2+54
+  1  arrangement 3 cells + !  \ 83-7
+  global-happiness 330 ?s
+  2  arrangement 0 cells + !  \ 60-62
+  0  arrangement 1 cells + !  \ -59-2
+  3  arrangement 2 cells + !  \ 46-7
+  1  arrangement 3 cells + !  \ -63-7
+  global-happiness 23 ?s
 }t
 bye
 
